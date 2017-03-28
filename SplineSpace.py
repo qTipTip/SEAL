da1e89789de1d99e933e4dfa4375a6dba40c6079
Ivar Stangeby <istangeby@gmail.com>
2017-03-29 01:12:51 +0200

    SplineSpace wrapper working as intended.

---
+++ SplineSpace.py
@@ -0,0 +1,67 @@
+from SplineFunction import SplineFunction
+import numpy as np
+
+class SplineSpace(object):
+    """
+    The SplineSpace object functions mostly as a wrapper around the SplineFunction
+    objects, providing auxiliary methods, mostly for visualization.
+    """
+    def __init__(self, p, t):
+        """
+        Initializes a spline space.
+        :param p: Spline degree, integer
+        :param t: knot vector of knots, ndarray
+        """
+
+        self.p = p
+        self.t = t
+        self.n = len(t) - p - 1
+
+        # TODO: Handle this properly
+        assert self.n > 0, "The knot vector has to be of at least length p + 2 = {p}".format(p=p+2)
+
+    def __call__(self, c):
+        """
+        Overrides the __call__ operator for the Spline Space object.
+        Given a set of coefficients of length self.n, returns a corresponding
+        SplineFunction object.
+        :param c: Set of spline coefficients, in arbitrary dimension >= 1
+        :return: SplineFunction object
+        """
+
+        return SplineFunction(self.p, self.t, c)
+
+    def parameter_values(self, resolution=100):
+        """
+        Returns a list of parameter values, uniformly spaced in the range t[0], t[n+p+1]
+        :param resolution: int, number of uniformly spaced values
+        :return: numpy array of :resolution: number of uniformly spaced values in the range t[0], t[n+p+1].
+        """
+
+        return np.linspace(self.t[0], self.t[-1], resolution)
+
+    @property
+    def basis(self):
+        """
+        :return: Returns the n B-splines as callable SplineFunction objects 
+        """
+
+        basis = []
+        for i in range(self.n):
+            c = [i == j for j in range(self.n)]
+            basis.append(SplineFunction(self.p, self.t, c))
+
+        return basis
+
+    def __str__(self):
+        """
+        Overides the __str__ method
+        :return: info-string
+        """
+
+        return """
+        Spline Space:
+            Dimension       = {n}
+            Degree          = {p}
+            Number of knots = {t}
+        """.format(n=self.n, p=self.p, t=len(self.t))
--- examples/spline_functions.py
+++ examples/spline_functions.py
@@ -1,31 +1,44 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
 
-from SplineFunction import SplineFunction
-from lib import knot_averages
+from SplineSpace import SplineSpace
 
 p = 2
 t = [0, 0, 0, 1, 2, 3, 4, 4, 4]
-n = len(t) - p - 1
+S = SplineSpace(p, t)
 
+# Parametric
 c = [(1, -1, 3), (2, 1, 4), (4, 4, 5), (4, 9, 0), (5, 3, 1), (6, 6, -2)]
-f = SplineFunction(p, t, c)
-x_values = np.linspace(t[0], t[-1], 200)
+f = S(c)
+x_values = S.parameter_values(100)
 y_values = f(x_values)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(*zip(*y_values))
-ax.plot(*zip(*f.control_polygon()))
-ax.scatter(*zip(*f.control_polygon()), s=50)
+ax.plot(*zip(*f.control_polygon))
+ax.scatter(*zip(*f.control_polygon), s=50)
 plt.show()
+
 
 c = [-1, 1, -1, 1, -1, 1]
-f = SplineFunction(p, t, c)
-x_values = np.linspace(t[0], t[-1], 200)
+f = S(c)
+x_values = S.parameter_values(100)
 y_values = f(x_values)
-poly = f.control_polygon()
+poly = f.control_polygon
 
+#
 plt.plot(x_values, y_values)
 plt.plot(*zip(*poly))
+plt.scatter(*zip(*poly))
 plt.show()
+
+B = S.basis
+for b in B:
+    y = b(x_values)
+    plt.plot(x_values, y)
+    poly = b.control_polygon
+    plt.plot(*zip(*poly))
+    plt.scatter(*zip(*poly))
+plt.show()
+
+print(S)
