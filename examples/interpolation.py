0bb1b46a08a536d8bad3459e07abde325c5a1be0
Ivar Stangeby <istangeby@gmail.com>
2017-04-01 14:02:45 +0200

    Updated interpolation example to show difference between approximated derivatives and supplied derivatives/

--- examples/interpolation.py
+++ examples/interpolation.py
@@ -3,17 +3,23 @@ import numpy as np
 
 from SEAL.interpolation import cubic_hermite_interpolation
 
-x_values = np.linspace(0, 2 * np.pi, 4)
+x_values = np.linspace(0, 2 * np.pi, 6)
 f_values = np.sin(x_values)
 df_values = np.cos(x_values)
 
 plt.scatter(x_values, f_values)
 
-data_set = np.column_stack((x_values, f_values, df_values))
+data_set_no_df = np.column_stack((x_values, f_values))
+data_set_df = np.column_stack((x_values, f_values, df_values))
 
-x_values = np.linspace(0, 2 * np.pi, 100)
-Hf = cubic_hermite_interpolation(data_set)
-Hf_values = Hf(x_values)
-plt.plot(x_values, np.sin(x_values))
-plt.plot(x_values, Hf_values)
+x_values = np.linspace(0, 2 * np.pi, 1000)
+Hf_df = cubic_hermite_interpolation(data_set_df)
+Hf_no_df = cubic_hermite_interpolation(data_set_no_df)
+Hf_no_df_values = Hf_no_df(x_values)
+Hf_df_values = Hf_df(x_values)
+f_values = np.sin(x_values)
+plt.plot(x_values, f_values, label='Exact')
+plt.plot(x_values, Hf_df_values, label='With derivatives')
+plt.plot(x_values, Hf_no_df_values, label='Without derivatives')
+plt.legend()
 plt.show()
