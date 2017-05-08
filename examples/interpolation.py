3cb2b614882632e760b32318fb31f7e48b8f9f61
Ivar Stangeby <istangeby@gmail.com>
2017-05-08 21:13:13 +0200

    Updated interpolation example

--- examples/interpolation.py
+++ examples/interpolation.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from SEAL.interpolation import cubic_hermite_interpolation
 
-x_values = np.linspace(0, 2 * np.pi, 6)
+x_values = np.linspace(0, 2 * np.pi, 4)
 f_values = np.sin(x_values)
 df_values = np.cos(x_values)
 
@@ -17,6 +17,7 @@ Hf_no_df_values = Hf_no_df(x_values)
 Hf_df_values = Hf_df(x_values)
 
 f_values = np.sin(x_values)
+
 plt.plot(x_values, f_values, label='Exact')
 plt.plot(x_values, Hf_df_values, label='With derivatives')
 plt.plot(x_values, Hf_no_df_values, label='Without derivatives')
