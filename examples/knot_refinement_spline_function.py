225fca8adc700f7abbfcf611091c6f57909b08f6
Ivar Stangeby <istangeby@gmail.com>
2017-05-10 14:43:33 +0200

    Example of knot refinement working in 3D

--- examples/knot_refinement_spline_function.py
+++ examples/knot_refinement_spline_function.py
@@ -1,12 +1,12 @@
 from SEAL import SplineFunction, SplineSpace
 
-import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 p = 2
 tau = [0, 0, 0, 1, 2, 2, 2]
 t = [0, 0, 0, 0.5, 1, 1.5, 2, 2, 2]
-c = [1, -1, 1, -1]
+c = [(1, 0, -1), (-1, 2, 2), (1, 5, -1), (-1, -1, 4)]
 S = SplineSpace(p, tau)
 
 f = S(c)
@@ -19,9 +19,12 @@ x = S.parameter_values()
 y = f(x)
 y_refined = f_refined(x)
 
-plt.plot(x, y)
-plt.plot(x, y_refined)
-plt.plot(*zip(*cp))
-plt.plot(*zip(*cp_refined))
+fig = plt.figure()
+ax = Axes3D(fig)
+
+ax.plot(*zip(*y))
+ax.plot(*zip(*y_refined))
+ax.plot(*zip(*cp))
+ax.plot(*zip(*cp_refined))
 plt.show()
 
