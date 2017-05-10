7f32c5fa3f659332ff8b8dae1b8186d145e32f76
Ivar Stangeby <istangeby@gmail.com>
2017-05-10 16:05:45 +0200

    Example of visualization using knot insertion and inserting middle points in each each non-trivial knot interval.

--- examples/visualization_using_refinement.py
+++ examples/visualization_using_refinement.py
@@ -1,15 +1,21 @@
-from SEAL import SplineSpace
+from SEAL import SplineSpace, create_knots
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 p = 2
-t = [0, 0, 0, 1, 2, 2, 2]
+n = 10
+t = create_knots(0, 1, p, n)
 S = SplineSpace(p, t)
 
-c = [1, -1, 1, -3]
+c = [(0, 1, 0), (1, 2, 1), (1.5, 3, 2), (1.7, -1, 3), (1, -1.5, 4), (3, 3, 3), (4, 4, 3), (5, 2, 2), (6, 5, 4), (7, -1, 5)]
 f = S(c)
+x = S.parameter_values()
+y = f(x)
 
-for i in range(10):
-    cp = f.visualize(iterations=i)
-    plt.plot(*zip(*cp), label="{}".format(i))
-    plt.legend()
-    plt.show()
+cp = f.visualize(iterations=4)
+fig = plt.figure()
+axs = Axes3D(fig)
+
+axs.plot(*zip(*cp))
+axs.plot(*zip(*y))
+plt.show()
